44106cf814f671d52dcad968e2ce0e11d388df9b
Kim Lu <{username}@users.noreply.github.com>
2024-01-09 16:33:27 -0500

    updated python file name and refactored code

--- langchain_streamlit_awsbr.py
+++ langchain_streamlit_awsbr.py
@@ -14,19 +14,20 @@ st.header("ChittyBot: AWS Bedrock 🌩️ + LangChain 🦜️🔗 + Streamlit �
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    print("--- Chat history initialized ---")
 
-if "bedrock_chat_object" not in st.session_state:
-    st.session_state.bedrock_chat_object = BedrockChat(
+if "llm" not in st.session_state:
+    st.session_state.llm = BedrockChat(
         model_id="anthropic.claude-v2", # AWS Bedrock Model ID
         model_kwargs={"temperature": 0.7, "max_tokens_to_sample": 1024}, # AWS Bedrock Model arguments
         streaming=True, # enable response streaming
         callbacks=[StreamingStdOutCallbackHandler()], # response streaming handler
         verbose=False
     )
-    print("Bedrock Chat Object Initialized")
+    print("--- LLM initialized ---")
 
-# Reuse Bedrock chat object stored in Streamlit session
-bedrock_chat = st.session_state.bedrock_chat_object
+# Reuse LLM object stored in Streamlit session
+llm = st.session_state.llm
 
 # Loop through messages list (chat history) and display in streamlit chat message container
 for message in st.session_state.messages:
@@ -38,26 +39,33 @@ for message in st.session_state.messages:
 # The string "Hello!" is used as a placeholder for the user's message.
 if prompt := st.chat_input("Hello!"):
     print(f"\nHuman: {prompt}")
-    print("\nAI: ")
+    print("AI: ")
+
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    # Add user message to session chat history
     human_input = HumanMessage(content=prompt, additional_kwargs={"role": "user"})
-    # Add user message to chat history
     st.session_state.messages.append(human_input)
-    # chat_history.append(HumanMessage(content=prompt))
     
-    stream_iterator = bedrock_chat.stream(st.session_state.messages)
+    # Stream response from LLM and display in chat message container
+    # Notice we send the list of messages so the bot remembers previous dialogues within the chat session
+    stream_iterator = llm.stream(st.session_state.messages)
     with st.chat_message("assistant"):
+        # Start with an empty message container for the assistant
         message_placeholder = st.empty()
+        
+        # Stream response from LLM in chunks and display in chat message container for real time chat experience
         full_response = ""
         for chunk in stream_iterator:
-            full_response += chunk.content + " "
-            
+            full_response += chunk.content + " "    
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         
+        # Add the final response to the chat history 
         ai_response = AIMessage(content=full_response, additional_kwargs={"role": "assistant"})
         st.session_state.messages.append(ai_response)
+        
+        # display in chat message container
         message_placeholder.markdown(full_response)
