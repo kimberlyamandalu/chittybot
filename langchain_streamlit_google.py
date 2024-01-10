29e36fcc400167e2331aab5c2a2b0bab71f68daa
Kim Lu <{username}@users.noreply.github.com>
2024-01-10 11:06:30 -0500

    cleaned up code

--- langchain_streamlit_google.py
+++ langchain_streamlit_google.py
@@ -1,6 +1,6 @@
 # Created a second version of ChittyBot, but this time, using Google Gemini Pro as the LLM.
 # The purpose is to show how LangChain makes it easy to change LLM model used without needing
-# to refactor the entire codebase. 
+# to refactor the entire codebase.
 
 import os
 import getpass
@@ -9,7 +9,7 @@ from langchain.schema import HumanMessage, AIMessage
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-    
+
 # Load environment variables from .env file
 ## DO NOT COMMIT .env --> ADD to .gitignore
 dotenv.load_dotenv()
@@ -38,11 +38,13 @@ llm = st.session_state.llm
 
 # Loop through messages list (chat history) and display in streamlit chat message container
 for message in st.session_state.messages:
-    # The "role" key in the additional_kwargs dict is used to determine the role icon (user or assistant) to use
+    # The "role" key in the additional_kwargs dict is used to determine the role icon 
+    # (user or assistant) to use
     with st.chat_message(message.additional_kwargs['role']):
         st.markdown(message.content)
 
-# st.chat_input creates a chat input box in the Streamlit app. The user can enter a message and it will be displayed in the chat message container.
+# st.chat_input creates a chat input box in the Streamlit app.
+# The user can enter a message and it will be displayed in the chat message container.
 # The string "Hello!" is used as a placeholder for the user's message.
 if prompt := st.chat_input("Hello!"):
     print(f"\nHuman: {prompt}")
@@ -57,13 +59,15 @@ if prompt := st.chat_input("Hello!"):
     st.session_state.messages.append(human_input)
     
     # Stream response from LLM and display in chat message container
-    # Notice we send the list of messages so the bot remembers previous dialogues within the chat session
+    # Notice we send the list of messages so the bot remembers previous dialogues 
+    # within the chat session
     stream_iterator = llm.stream(st.session_state.messages)
     with st.chat_message("assistant"):
         # Start with an empty message container for the assistant
         message_placeholder = st.empty()
-        
-        # Stream response from LLM in chunks and display in chat message container for real time chat experience
+
+        # Stream response from LLM in chunks and display in chat message container 
+        # for real time chat experience
         full_response = ""
         for chunk in stream_iterator:
             full_response += chunk.content + " "
@@ -72,7 +76,10 @@ if prompt := st.chat_input("Hello!"):
             message_placeholder.markdown(full_response + "▌")
         
         # Add the final response to the chat history 
-        ai_response = AIMessage(content=full_response, additional_kwargs={"role": "assistant"})
+        ai_response = AIMessage(
+            content=full_response,
+            additional_kwargs={"role": "assistant"}
+        )
         st.session_state.messages.append(ai_response)
         
         # display in chat message container
